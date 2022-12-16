5100762dde16e475ac0844fd219ff8eb6a05c053
MartinKitko <martin.kitko736@gmail.com>
2022-12-16 11:34:00 +0100

    prvy commit

---
+++ cmake-build-debug/CMakeFiles/AnonSietKlient.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AnonSietKlient"
+  "AnonSietKlient.pdb"
+  "CMakeFiles/AnonSietKlient.dir/definitions.c.o"
+  "CMakeFiles/AnonSietKlient.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AnonSietKlient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
