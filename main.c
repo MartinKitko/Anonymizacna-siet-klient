a95da9cde93f7d3229a610b7999d56155f3502b2
MartinKitko <martin.kitko736@gmail.com>
2022-12-21 17:11:37 +0100

    pridane prepojenie klienta zo serveru na entry node

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -33,14 +34,32 @@ int main(int argc, char *argv[]) {
 
     //definovanie adresy servera <arpa/inet.h>
     struct sockaddr_in serverAddress;
-    bzero((char *)&serverAddress, sizeof(serverAddress));
+    bzero((char *) &serverAddress, sizeof(serverAddress));
     serverAddress.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, (char *)&serverAddress.sin_addr.s_addr, server->h_length);
+    bcopy((char *) server->h_addr, (char *) &serverAddress.sin_addr.s_addr, server->h_length);
     serverAddress.sin_port = htons(port);
 
-    if (connect(sock,(struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
-        printError("Chyba - connect.");
+    if (connect(sock, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) < 0) {
+        printError("Chyba - server connect.");
     }
+
+    struct sockaddr_in entryNodeAddress;
+    recv(sock, &entryNodeAddress, sizeof(entryNodeAddress), 0);
+    close(sock);
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        printError("Chyba - socket.");
+    }
+
+    if (connect(sock, (struct sockaddr *) &entryNodeAddress, sizeof(entryNodeAddress)) < 0) {
+        printError("Chyba - node connect.");
+    }
+
+    /*char msg[1024];
+    sprintf(msg, "Test: URL poslane klientom.");
+    if (send(sock, msg, sizeof(msg), 0) < 0) {
+        printError("Chyba - send.");
+    }*/
 
     //inicializacia dat zdielanych medzi vlaknami
     DATA data;
@@ -48,10 +67,10 @@ int main(int argc, char *argv[]) {
 
     //vytvorenie vlakna pre zapisovanie dat do socketu <pthread.h>
     pthread_t thread;
-    pthread_create(&thread, NULL, data_writeData, (void *)&data);
+    pthread_create(&thread, NULL, data_writeData, (void *) &data);
 
     //v hlavnom vlakne sa bude vykonavat citanie dat zo socketu
-    data_readData((void *)&data);
+    data_readData((void *) &data);
 
     //pockame na skoncenie zapisovacieho vlakna <pthread.h>
     pthread_join(thread, NULL);
